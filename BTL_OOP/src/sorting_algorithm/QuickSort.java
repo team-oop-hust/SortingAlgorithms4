07d6f86bec632e7a54bc606ffc1c4c6fd02775e0
PoorHust <105424446+PoorHust@users.noreply.github.com>
2022-06-23 16:21:39 +0700

    Update QuickSort.java

--- BTL_OOP/src/sorting_algorithm/QuickSort.java
+++ BTL_OOP/src/sorting_algorithm/QuickSort.java
@@ -10,18 +10,18 @@ import java.util.Random;
 import javax.swing.JPanel;
 
 public class QuickSort extends BaseSort{
-	//
 	int delay=30;
-	public void Progress(int low, int high) {
+	public void Progress(int low, int high,Element[]arr) {
 		int j=high;
-		Coloring(elements[high],red);
+		Coloring(arr[high],red);
 		for(int i=0;i<high;i++) {
-			if(elements[i].getValue()<elements[j].getValue()) {
-				Swap(i);j--;
+			if(arr[i].getValue()<arr[j].getValue()) {
+				Swap(i,arr);j--;
 			}
 		}
 		Progress(low,j-1);
 		Progress(j+1,high);
+		if(high=1) break;
 		}
 	public void Move(int p, Element[]arr)
 	{
@@ -29,22 +29,14 @@ public class QuickSort extends BaseSort{
 		{
 			Point[] waypoint = new Point[3];
 			int High = arr.length-1;
-			yTmp=arr[p].getPosition().y+50;
+			int yTmp=arr[p].getPosition().y+50;
 			waypoint[0] = new Point(arr[p].getPosition().x, yTemp);
 			waypoint[1] = new Point(arr[High].getPosition().x, yTmp);
 			waypoint[2] = new Point(arr[High].getPosition().x,arr[High].getPosition().y);
 			for(int i=p+1;p<High;p++) {
 			Point[] temp = new Point[1];
 			temp[0] = new Point(arr[i-1].getPosition().x,arr[i-1].getPosition().y);
-			int index0=0;
-			while(index0 < temp.length)
-			{
-			arr[i].setPosition(MoveTowards(arr[i].getPosition(),temp, 10));}
-			if(arr[i].getPosition().equals(temp[index0]))
-				{
-					index0++;
-				}
-			Thread.sleep(delay);
+			arr[i].setPosition(MoveTowards(arr[i].getPosition(),temp, 10));
 		}	
 			int index = 0;
 			while(index < waypoint.length)
@@ -63,20 +55,20 @@ public class QuickSort extends BaseSort{
 		}
 	}
 
-	public void Swap(int p)
+	public void Swap(int p,Element[]arr)
 	{
-		int High=elements.lenght-1;
-		Element temp=elements[p];
+		int High=arr.lenght-1;
+		Element temp=arr[p];
 		for(int i=p+1;p<High;p++) {
-		elements[i] = elements[i+1];
+		arr[i] = arr[i+1];
 		}
-		elements[High]=temp;
-		this.Move(p,elements);
+		arr[High]=temp;
+		this.Move(p,arr);
 	}
     public void Sort() {
     	try {
     		int high=elements.lenght-1;
-    		Progress(0,high);
+    		Progress(0,high,elements);
     		}
     		catch(Exception ex)
     		{
