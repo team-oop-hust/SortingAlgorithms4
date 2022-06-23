f2286c47658a9b207b3e835ed60803435317cd91
PoorHust <105424446+PoorHust@users.noreply.github.com>
2022-06-23 18:54:55 +0700

    Update QuickSort.java

--- BTL_OOP/src/sorting_algorithm/QuickSort.java
+++ BTL_OOP/src/sorting_algorithm/QuickSort.java
@@ -2,82 +2,76 @@ package sorting_algorithm;
 
 import base.BaseSort;
 import base.Element;
-import base.element;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.util.Random;
 import javax.swing.JPanel;
 
+
 public class QuickSort extends BaseSort{
-	int delay=30;
-	public void Progress(int low, int high,Element[]arr) {
-		int j=high;
-		Coloring(arr[high],red);
-		for(int i=0;i<high;i++) {
-			if(arr[i].getValue()<arr[j].getValue()) {
-				Swap(i,arr);j--;
-			}
-		}
-		Progress(low,j-1);
-		Progress(j+1,high);
-		if(high=1) break;
-		}
-	public void Move(int p, Element[]arr)
+	public QuickSort(JPanel container) {
+		super(container);
+		this.InitRandomArray(15, 1000);
+	}
+	
+	public void Sort() {
+		int high=this.elements.length;
+		this.Progress(this.elements,0,high);
+	}
+	public void Progress(Element[] elements,int low, int high){
+    int pivot = elements[high].getValue();
+    int i = low - 1;
+    for(int j = low; j <= high - 1; j++)
+    {
+        if (elements[j].getValue() < pivot)
+        {
+            i++;
+            this.Swap(i,j);
+        }
+    }
+    this.Swap(i + 1, high);
+	Progress(elements,low, i);
+    Progress(elements,i+2, high);
+}
+ 
+	public void Move(Element e, Element f, int delay)
 	{
 		try 
 		{
-			Point[] waypoint = new Point[3];
-			int High = arr.length-1;
-			int yTmp=arr[p].getPosition().y+50;
-			waypoint[0] = new Point(arr[p].getPosition().x, yTemp);
-			waypoint[1] = new Point(arr[High].getPosition().x, yTmp);
-			waypoint[2] = new Point(arr[High].getPosition().x,arr[High].getPosition().y);
-			for(int i=p+1;p<High;p++) {
-			Point[] temp = new Point[1];
-			temp[0] = new Point(arr[i-1].getPosition().x,arr[i-1].getPosition().y);
-			arr[i].setPosition(MoveTowards(arr[i].getPosition(),temp, 10));
-		}	
-			int index = 0;
+			Point[] waypoint = new Point[4];
+			int yTmp = e.getPosition().y + 50;
+			waypoint[0] = new Point(e.getPosition().x, e.getPosition().y);
+			waypoint[1] = new Point(e.getPosition().x, yTmp);
+			waypoint[2] = new Point(f.getPosition().x, yTmp);
+			waypoint[3] = new Point(f.getPosition().x, f.getPosition().y);
+			int index = 1;
 			while(index < waypoint.length)
 			{
-				arr[p].setPosition(MoveTowards(arr[p].getPosition(),waypoint, 10));
-				if(e.getPosition().equals(waypoint[index]))
+				e.setPosition(MoveTowards(e.getPosition(), waypoint[index], 10));
+				f.setPosition(MoveTowards(f.getPosition(), waypoint[waypoint.length-1-index], 10));
+				if(f.getPosition().equals(waypoint[waypoint.length-1-index]))
 				{
 					index++;
 				}
-			Thread.sleep(delay);
+				Thread.sleep(delay);
+			}
 		}
-	}
 		catch(Exception ex)
 		{
 			ex.printStackTrace();
 		}
 	}
-
-	public void Swap(int p,Element[]arr)
+	
+	public void Swap(int e1, int e2)
 	{
-		int High=arr.lenght-1;
-		Element temp=arr[p];
-		for(int i=p+1;p<High;p++) {
-		arr[i] = arr[i+1];
-		}
-		arr[High]=temp;
-		this.Move(p,arr);
-	}
-    public void Sort() {
-    	try {
-    		int high=elements.lenght-1;
-    		Progress(0,high,elements);
-    		}
-    		catch(Exception ex)
-    		{
-    			ex.printStackTrace();
-    		}
-    		ResetValue();
-    	}
-	public QuickSort(JPanel container) {
-		super(container);
-		this.InitRandomArray(15, 1000);
+		Coloring(elements[e1], Color.yellow);
+		Coloring(elements[e2], Color.yellow);
+		Element tmp = elements[e1];
+		elements[e1] = elements[e2];
+		elements[e2] = tmp;
+		this.Move(elements[e1],elements[e2], 30);
+		Coloring(elements[e1], Color.blue);
+		Coloring(elements[e2], Color.blue);
 	}
 }
