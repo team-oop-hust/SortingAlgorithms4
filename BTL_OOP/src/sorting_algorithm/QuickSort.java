8c7f4e81e251114757b84f03c4d9e9f2ce3657fe
PoorHust <105424446+PoorHust@users.noreply.github.com>
2022-07-05 18:27:48 +0700

    Update QuickSort.java

--- BTL_OOP/src/sorting_algorithm/QuickSort.java
+++ BTL_OOP/src/sorting_algorithm/QuickSort.java
@@ -15,10 +15,14 @@ public class QuickSort extends BaseSort {
 	
 	public void SortIncrease() {
 		int high = this.elements.length -1;
-		this.Progress(0, high);
+		this.Progress1(0, high);
+	}
+	public void SortDecrease() {
+		int high = this.elements.length -1;
+		this.Progress2(0, high);
 	}
 	
-	public int partition(int low, int high)
+	public int partition1(int low, int high)
 	{
 		Coloring(elements[high], Color.red);
 	    int pivot = elements[high].getValue();
@@ -35,14 +39,39 @@ public class QuickSort extends BaseSort {
 	    Coloring(elements[high], Color.blue);
 	    return (i + 1);
 	}
-
-	public void Progress(int low, int high) {
+	public void Progress1(int low, int high) {
 		
 		if (low < high)
 	    {
-	        int pi = partition(low, high);
-			Progress(low, pi - 1);
-			Progress(pi + 1, high);
+	        int pi = partition1(low, high);
+			Progress1(low, pi - 1);
+			Progress1(pi + 1, high);
+	    }
+	}
+	public int partition2(int low, int high)
+	{
+		Coloring(elements[high], Color.red);
+	    int pivot = elements[high].getValue();
+	    int i = (low - 1);
+	    for(int j = low; j <= high - 1; j++)
+	    {
+	        if (elements[j].getValue() > pivot)
+	        {
+	            i++;
+	            Swap(i, j);
+	        }
+	    }
+	    Swap(i + 1, high);
+	    Coloring(elements[high], Color.blue);
+	    return (i + 1);
+	}
+	public void Progress2(int low, int high) {
+		
+		if (low < high)
+	    {
+	        int pi = partition2(low, high);
+			Progress2(low, pi - 1);
+			Progress2(pi + 1, high);
 	    }
 	}
 }
